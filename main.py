d43be14a28e2e9d4fc192539cf8ebcb8672e40ad
Daniel <dsokoler@purdue.edu>
2017-03-22 12:35:32 -0400

    Can find first instance of function call and the function that call is inside of, but errors after that. PROGRESS!

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ methodQueue = [];	#Queue of tuples (methodName, methodNode) of methods we're tra
 rootNode 	= None;	#The root of our tree
 currentNode = rootNode;
 
+
 class CFGNode():
 	"""
 	Currently represents a function call
@@ -51,7 +52,10 @@ class FuncCallVisitor(c_ast.NodeVisitor):
 		"""Store information we'll need here"""
 		self.funcname 		= funcname
 		self.current_parent = None;
+
+		global rootNode;
 		self.currentCFGNode = rootNode;
+		self.parentList = [];	#Keeps track of the list of nodes we've seen on this path in the AST (works b/c generic_visit is DFS)
 
 	def visit_FuncCall(self, node):
 		"""Triggers every time we find a FuncCall node in PyCParser's AST"""
@@ -59,33 +63,56 @@ class FuncCallVisitor(c_ast.NodeVisitor):
 		if node.name.name == self.funcname:
 			#Check if this method is already in the parent node's children, if so we don't need to add it again, if not add it
 			#NOTE: We probably want to know all calls inside a method as well as the line numbers those calls are on
-			#	 : Line numbers for the calls are located in the FuncCall node's coord field
-			if ( any(x.function == node.name.name for x in self.currentCFGNode.parents) ):
-				return;
-			self.currentCFGNode.add_parent( CFGNode(node.name.name, node) );
+			if ( not any(x.function == node.name.name for x in self.currentCFGNode.parents) ):
+				#self.currentCFGNode.add_parent( CFGNode(node.name.name, node) );
+				
+				#Upwards trace of c_ast nodes until we find the FuncDef that 'node' is inside of
+				numberAboveCurrent = -1;
+				isDefinedIn = self.parentList[numberAboveCurrent];
+				while (not isinstance(isDefinedIn, c_ast.FuncDef)):
+					if (isinstance(isDefinedIn, c_ast.FileAST)):	#If we get to the top of the AST something really bad happened
+						print("ERROR (FATAL): upward parent trace reached FileAST node");
+						sys.exit();
 
-			#Find method this node is in (e.g. what method called this one) (our "new" method)
-			methodName, isDefinedIn = getNameAndASTNode(node);
+					numberAboveCurrent -= 1;
+					isDefinedIn = self.parentList[numberAboveCurrent];
 
-			#Make a CFG node for this "new" node, and add it to the methodQueue only if it is not already in the methodQueue
-			if (not any(entry[0] == methodName for entry in methodQueue) ):
-				newNode = CFGNode(methodName, isDefinedIn);			#Make the new CFGNode
-				currentNode.add_child(newNode);						#Add the new CFGNode as a child of the current CFGNode
-				newNode.add_parent(currentNode);					#Add the current CFGNode as a parent of the new CFGNode
-				methodQueue.append( (methodName, newNode) );		#Add the method we found it in to the methodQueue
+				#Get the name and location of the function we are in
+				methodName = isDefinedIn.decl.name;
+				methodLocation = isDefinedIn.decl.coord;
 
-			print('%s called at %s' % (self.funcname, node.name.coord))
+				#Something really bad happened for us to not find the name for this FuncDef node
+				if (methodName is None):
+					print("ERROR (FATAL): unable to locate function name holding call to " + self.funcname);
+					sys.exit();
+
+				#Make a CFG node for this "new" node, and add it to the methodQueue only if it is not already in the methodQueue
+				if (not any(entry[0] == methodName for entry in methodQueue) ):
+					newNode = CFGNode(methodName, isDefinedIn);			#Make the new CFGNode
+					self.currentCFGNode.add_child(newNode);				#Add the new CFGNode as a child of the current CFGNode
+					newNode.add_parent(currentNode);					#Add the current CFGNode as a parent of the new CFGNode
+					methodQueue.append( (methodName, newNode) );		#Add the method we found it in to the methodQueue
+
+					self.currentCFGNode = newNode;
+
+				print('%s called at %s inside %s declared at %s' % (self.funcname, node.name.coord, methodName, methodLocation))
+
+		#Visit all children of this node
+		FuncCallVisitor.generic_visit(self, node);
 
 	def generic_visit(self, node):
-		"""Overrides the standard generic_visit method to include parent links, accessed while traversing"""
-		node.parent = self.current_parent;	#Assigns a parent field to the pycparser node
+		"""Overrides the standard generic_visit method to keep track of the parent listings,
+		   which are accessed while traversing"""
 
-		#Taken from PyCParser github FAQ
-		oldparent = self.current_parent
-		self.current_parent = node
-		for c in node.children():
-			self.visit(c)
-		self.current_parent = oldparent
+		#We are about to move down another level, so add this node to the parentList
+		self.parentList.append(node);
+		
+		#Go through all of this node's children
+		for c_name, c in node.children():
+			self.visit(c);
+
+		#We just finished all children, and are about to move up a level, so remove this node from the parentList
+		self.parentList.pop(-1);
 
 
 class LineNumberVisitor(c_ast.NodeVisitor):
@@ -94,57 +121,39 @@ class LineNumberVisitor(c_ast.NodeVisitor):
 	def __init__(self, linenumber):
 		self.lineno 	= linenumber;	#The line number we are looking for
 		self.ast_node 	= None;			#The node found on the specified linenumber
+		self.lastFuncDefName = None;	#The name of the function this line number is inside of
+		self.lastFuncDefNode = None;	#The FuncDef node this line number is inside of
 
 	def generic_visit(self, node):
 		"""Overrides the standard generic_visit to find the node on the specified line number"""
 
 		#We will never need to find the FileAST node
-		if (isinstance(node, c_ast.FileAST)):
-			return;
+		if (not isinstance(node, c_ast.FileAST)):
+			#Keeps track of the function we are currently inside of
+			if (isinstance(node, c_ast.FuncDef)):
+				self.lastFuncDefNode = node;
 
-		#Don't do anything if we've already found a node on that line number
-		if (self.ast_node is not None):
-			return;
+				#Find the name of the function (based on the above processing) that 'node' is inside of
+				for c_name, child in node.children():	#Now we have the FuncDef node of the method we are in
+					if (isinstance(child, c_ast.Decl)):			#Found where the node's name is stored
+						self.lastFuncDefName = child.name;
+						break;
 
-		#Figure out if we have a node from that line number
-		node.show(showcoord=True);
-		lineNumber = lineNumberFromCoord(node.coord)
-		if (lineNumber is not None and lineNumber == self.lineno):
-			self.ast_node = node;
+			#Don't do anything if we've already found a node on that line number
+			if (self.ast_node is not None):
+				return;
 
+			#Figure out if we have a node from that line number
+			lineNumber = node.coord.line;
+			if (lineNumber is not None and lineNumber == self.lineno):
+				print("Located node on line " + str(lineNumber));
+				self.ast_node = node;
+				return;
 
-def getNameAndASTNode(node):
-	"""Returns the name of the method the specified node is in, along with its AST node"""
-
-	#Upwards trace of c_ast nodes until we find the FuncDef that 'node' is inside of
-	isDefinedIn = node.parent;
-	while (not isinstance(isDefinedIn, c_ast.FuncDef)):
-		if (isinstance(isDefinedIn, c_ast.FileAST)):	#If we get to the top of the AST something really bad happened
-			print("ERROR (FATAL): upward parent trace reached FileAST node");
-			sys.exit();
-
-		isDefinedIn = node.parent;
-
-	#Find the name of the function (based on the above processing) that 'node' is inside of
-	for i, child in enumerate(isDefinedIn or []):	#Now we have the FuncDef node of the method we are in
-		if (isinstance(child, c_ast.Decl)):			#Found where the node's name is stored
-			methodName = child.name.name;
-
-	return (methodName, isDefinedIn);
-
-
-def lineNumberFromCoord(coord):
-	"""Converts a PyCParser coord to the specified line number"""
-	lineNo = None;
-
-	#Ensure we don't get any unwanted errors
-	try:
-		#Coord is 'file.c:lineNumber'
-		lineNo = int(coord.split(':')[1]);
-	except ValueError:
-		print("ERROR: unable to convert coord to line number");
-
-	return lineNo
+		#If this node didn't give us the line number, go to all its children
+		if (self.ast_node is None):
+			for c_name, c in node.children():
+				self.visit(c);
 
 
 #
@@ -155,24 +164,25 @@ def lineNumberFromCoord(coord):
 #
 def parseForCFG(filename, lineNo):
 	"""Parse the file filename for a Control Flow Graph starting at lineNo"""
+	global rootNode;
 	rootNode = CFGNode("", None)
 
 	#Create the AST to parse
 	ast = parse_file(filename, use_cpp=True)
 
 	#Given the line number, find the node of that line number
-	v = LineNumberVisitor(lineNo);
-	v.visit(ast);
-	vulnerableNode = v.ast_node;	#The node on the specified line
+	lnv = LineNumberVisitor(lineNo);
+	lnv.visit(ast);
+	vulnerableNode = lnv.ast_node;	#The node on the specified line
 	if (vulnerableNode == None):
 		print("ERROR (FATAL): unable to retrieve node for line " + str(lineno));
 		sys.exit();
 
 	#Trace that upwards once to get the (methodName, methodNode) that the specified line is inside of
-	methodTuple = getNameAndASTNode(vulnerableNode);
+	
 
 	#Add those to the methodQueue
-	methodQueue.append( methodTuple );
+	methodQueue.append( (lnv.lastFuncDefName, lnv.lastFuncDefNode) );
 
 	#Parse continually while we have methods to look for in the methodQueue
 	while (methodQueue):
