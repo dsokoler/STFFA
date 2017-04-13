2d9946486db80ecbdcc9dbb705f798d373c1e6df
Daniel <dsokoler@purdue.edu>
2017-04-13 15:17:43 -0400

    Updated test file

--- testCFile.c
+++ testCFile.c
@@ -4,9 +4,21 @@ void foobar();
 
 int main(int argc, char* argv[]) {
 	int x = 5;
-
+	int y = 6;
+	int z = 7;
+	
 	if (x > 10) {
 		foo(argv[1]);
+	}
+	//Not finding this foo??
+	else if (y < 10) {
+		foo(argv[1]);
+	}
+	else if (x > 10 && y < 10) {
+		bar(argv[1]);
+	}
+	else if ( (x > 10 || y > 1) && z == 1 ) {
+		bar(argv[1]);
 	}
 	else {
 		bar(argv[1]);
